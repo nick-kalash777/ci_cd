1c4b363e65ce2a8962c59f2eb146f1d4b00a6947
nick-kalash777 <nick.kalash777@gmail.com>
2024-12-17 01:34:55 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# Установить Java
-echo "Installing Java 17..."
-sudo apt-get update
-sudo apt-get install -y openjdk-17-jdk
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+# Установить Java (скачать и распаковать OpenJDK)
+echo "Installing Java..."
+curl -L https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.8+7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.8_7.tar.gz -o java17.tar.gz
+mkdir -p java
+tar -xzf java17.tar.gz -C java --strip-components=1
+export JAVA_HOME=$(pwd)/java
 export PATH=$JAVA_HOME/bin:$PATH
+java -version
 
-# Установить Maven
+# Установить Maven (скачать и распаковать)
 echo "Installing Maven..."
-sudo apt-get install -y maven
+curl -L https://downloads.apache.org/maven/maven-3/3.9.4/binaries/apache-maven-3.9.4-bin.tar.gz -o maven.tar.gz
+mkdir -p maven
+tar -xzf maven.tar.gz -C maven --strip-components=1
+export MAVEN_HOME=$(pwd)/maven
+export PATH=$MAVEN_HOME/bin:$PATH
+mvn -version
 
 # Сборка проекта
 echo "Building Java project..."
