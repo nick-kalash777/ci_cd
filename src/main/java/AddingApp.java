a5e136db84993d2314b4c1820c1f3e93127b7d4c
nick-kalash777 <nick.kalash777@gmail.com>
2024-12-17 01:45:46 +0300

    app

--- src/main/java/AddingApp.java
+++ src/main/java/AddingApp.java
@@ -1,4 +1,7 @@
 public class AddingApp {
+    public static void main(String[] args) {
+        System.out.println("Deploy successful");
+    }
     public int add(int a, int b) {
         return a + b;
     }
