ec41288a5af6d4bd6593fc26ca0c0394d4f73bc9
nick-kalash777 <nick.kalash777@gmail.com>
2024-12-17 01:55:36 +0300

    app

--- src/main/java/AddingApp.java
+++ src/main/java/AddingApp.java
@@ -1,6 +1,31 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
 public class AddingApp {
     public static void main(String[] args) {
-        System.out.println("Deploy successful");
+        // Устанавливаем порт для прослушивания
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+            server.createContext("/", new HttpHandler() {
+                @Override
+                public void handle(HttpExchange exchange) throws IOException {
+                    String response = "Hello, Java!";
+                    exchange.sendResponseHeaders(200, response.getBytes().length);
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();
+                }
+            });
+            server.start();
+            System.out.println("Server started at port 8080...");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     public int add(int a, int b) {
         return a + b;
