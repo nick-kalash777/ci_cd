b53633515335ed35031582e9409581fbb4483850
nick-kalash777 <nick.kalash777@gmail.com>
2024-12-17 03:47:06 +0300

    app

--- src/main/java/AddingApp.java
+++ src/main/java/AddingApp.java
@@ -1,8 +1,6 @@
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.nio.charset.StandardCharsets;
 
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
@@ -10,35 +8,13 @@ import com.sun.net.httpserver.HttpExchange;
 
 public class AddingApp {
     public static void main(String[] args) {
+        // Устанавливаем порт для прослушивания
         try {
             HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
             server.createContext("/", new HttpHandler() {
                 @Override
                 public void handle(HttpExchange exchange) throws IOException {
-                    // Чтение данных из запроса
-                    InputStream requestBody = exchange.getRequestBody();
-                    String query = new String(requestBody.readAllBytes(), StandardCharsets.UTF_8);
-
-                    // Разбор чисел из строки запроса
-                    String[] parameters = query.split("&");
-                    int a = 0;
-                    int b = 0;
-                    for (String parameter : parameters) {
-                        String[] keyValue = parameter.split("=");
-                        if (keyValue.length == 2) {
-                            if (keyValue[0].equals("a")) {
-                                a = Integer.parseInt(keyValue[1]);
-                            } else if (keyValue[0].equals("b")) {
-                                b = Integer.parseInt(keyValue[1]);
-                            }
-                        }
-                    }
-
-                    // Выполнение сложения
-                    int result = add(a, b);
-
-                    // Формирование ответа
-                    String response = "Result: " + result;
+                    String response = String.valueOf(add(2, 4));
                     exchange.sendResponseHeaders(200, response.getBytes().length);
                     OutputStream os = exchange.getResponseBody();
                     os.write(response.getBytes());
@@ -51,7 +27,6 @@ public class AddingApp {
             e.printStackTrace();
         }
     }
-
     public static int add(int a, int b) {
         return a + b;
     }
